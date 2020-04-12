2fc6eb64f7e91581856ab8d7466883bbc96e4b02
ShantanuPhadke1996 <shantanu.phadke@berkeley.edu>
2020-04-12 13:52:24 -0700

    Updates

---
+++ precache-manifest.51f0c3f7284b04896e9407c605d003af.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a206c7da6dc0581c4b4fb2c3664b8c",
+    "url": "/www.naturomind.com/index.html"
+  },
+  {
+    "revision": "c561db919452df518e2c",
+    "url": "/www.naturomind.com/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8c06f79206ef0c62aa58",
+    "url": "/www.naturomind.com/static/css/main.868aef7b.chunk.css"
+  },
+  {
+    "revision": "c561db919452df518e2c",
+    "url": "/www.naturomind.com/static/js/2.fe31cc1a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/www.naturomind.com/static/js/2.fe31cc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c06f79206ef0c62aa58",
+    "url": "/www.naturomind.com/static/js/main.3fcc0ad8.chunk.js"
+  },
+  {
+    "revision": "3b225bd30806da945e69",
+    "url": "/www.naturomind.com/static/js/runtime-main.29953791.js"
+  },
+  {
+    "revision": "429fc6dff05763914c2838d6f1fdd5c8",
+    "url": "/www.naturomind.com/static/media/home_background.429fc6df.jpg"
+  },
+  {
+    "revision": "523805900679a2a7ed884fb3f0890b05",
+    "url": "/www.naturomind.com/static/media/home_diet_plan.52380590.jpg"
+  },
+  {
+    "revision": "7e5543ee4b56ce6315650b3c458b6eee",
+    "url": "/www.naturomind.com/static/media/home_holistic_approach.7e5543ee.jpg"
+  },
+  {
+    "revision": "9bd8b547eb7983c030624859ee13f7ff",
+    "url": "/www.naturomind.com/static/media/home_hydrotherapy.9bd8b547.jpg"
+  },
+  {
+    "revision": "dddf540936ec8c9d3285778784216716",
+    "url": "/www.naturomind.com/static/media/home_jogging.dddf5409.jpg"
+  },
+  {
+    "revision": "a03f5b3416433acb5675e52137accd97",
+    "url": "/www.naturomind.com/static/media/home_total_body_detox.a03f5b34.jpg"
+  },
+  {
+    "revision": "7e874eb278ec8be5864b9ab5dfd00d45",
+    "url": "/www.naturomind.com/static/media/home_yoga.7e874eb2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97fa3f1ec8f56e298c99a30acb3374e6.js"
+  "/www.naturomind.com/precache-manifest.51f0c3f7284b04896e9407c605d003af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.naturomind.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
