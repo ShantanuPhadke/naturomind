e5525186d795661c1cdd3c7b361a7f47b71c18a5
ShantanuPhadke1996 <shantanu.phadke@berkeley.edu>
2020-06-27 21:11:02 -0700

    Updates

---
+++ precache-manifest.e0a5ef9122f08c4b8a8f4996122b4605.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bfb69508b21c72bd04ad8439fbae3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b2dd3b848274fda1cf",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d0e21b536f863bf3ebef",
+    "url": "/static/css/main.82c7ba7c.chunk.css"
+  },
+  {
+    "revision": "21b2dd3b848274fda1cf",
+    "url": "/static/js/2.a800f213.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a800f213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e21b536f863bf3ebef",
+    "url": "/static/js/main.4baffb15.chunk.js"
+  },
+  {
+    "revision": "50d39c078fce6f4d3d23",
+    "url": "/static/js/runtime-main.8d0d2bed.js"
+  },
+  {
+    "revision": "8a3ffb631d1295c828d29f101c9998ce",
+    "url": "/static/media/blue-sky-clear-sky-cold-fog.8a3ffb63.jpg"
+  },
+  {
+    "revision": "429fc6dff05763914c2838d6f1fdd5c8",
+    "url": "/static/media/home_background.429fc6df.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3137ede322a4b64432bd5c2a02165955.js"
+  "/precache-manifest.e0a5ef9122f08c4b8a8f4996122b4605.js"
 );
 
 self.addEventListener('message', (event) => {
